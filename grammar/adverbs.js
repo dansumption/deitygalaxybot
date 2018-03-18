b951927856acb44ad46c300f32438216f5481da9
Dan Sumption <dan@sumption.org>
2018-03-18 19:49:50 +0000

    Moar adverbs

--- grammar/adverbs.js
+++ grammar/adverbs.js
@@ -1,8 +1,14 @@
 
 
 const adverbs = Array(10).fill(" ");
-const simpleAdverb = ["angrily", "innocently", "kindly", "mysteriously"];
-const complexAdverb = ["after a pause of a thousand years", "for no good reason", "in a funny voice", "on the spur of the moment", "out of pure malevolence"];
+const simpleAdverb = ["angrily", "innocently", "kindly",
+  "mysteriously", "impishly", "curiously", "malevolently", "imperiously",
+  "furiously", "gently", "smoothly", "cantankerously", "avuncularly"
+];
+const complexAdverb = ["after a pause of a thousand years", "for no good reason",
+  "in a funny voice", "on the spur of the moment",
+  "out of pure malevolence", "in the space between being and nothingness"
+];
 
 const commandAdverb = adverbs
   .concat(simpleAdverb.map(adverb => ` ${adverb} `))
