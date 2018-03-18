6c7da53eb4e54eb04f9ae464fa9407a1a0a50a24
Dan Sumption <dan@sumption.org>
2018-03-18 12:25:03 +0000

    Fix credentials

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const Twit = require('twit');
 const TwitterBot = require('node-twitterbot').TwitterBot;
 const config = require('./config');
 
-const Bot = new TwitterBot(config);
+const bot = new TwitterBot({
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+ });
 const phraseArray = ["A new deity is born.",
   "A long time ago, in a Deity Galaxy closer than you think...",
   "Deploy deities!"];
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
-const phrase = chooseRandom(phraseArray) + " " + chooseRandom(phraseArray);
-Bot.tweet(phrase);
+const phrase = chooseRandom(phraseArray);
+bot.tweet(phrase);
