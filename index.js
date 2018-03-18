b39fa2198184fce08fde04357a07fb11d61bafa5
Dan Sumption <dan@sumption.org>
2018-03-18 14:00:17 +0000

    Add tracery

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Twit = require('twit');
 const TwitterBot = require('node-twitterbot').TwitterBot;
+const tracery = require('./tracery/tracery');
+const modifiers = require('./tracery/mods-eng-basic');
+const origin = require('./grammar/origin');
 
 const bot = new TwitterBot({
   consumer_key: process.env.BOT_CONSUMER_KEY,
@@ -7,11 +10,10 @@ const bot = new TwitterBot({
   access_token: process.env.BOT_ACCESS_TOKEN,
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
  });
-const phraseArray = ["A new deity is born.",
-  "A long time ago, in a Deity Galaxy closer than you think...",
-  "Deploy deities!"];
-function chooseRandom(myArray) {
-  return myArray[Math.floor(Math.random() * myArray.length)];
-}
-const phrase = chooseRandom(phraseArray);
+
+const grammar = tracery.createGrammar(origin);
+grammar.addModifiers(modifiers);
+
+const phrase = grammar.flatten('#origin#');
+
 bot.tweet(phrase);
