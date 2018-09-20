bb43338e9c51e016c287ee0149b9c1ec643b0492
Dan Sumption <dan@sumption.org>
2018-09-20 22:34:31 +0100

    Add defeat-a-relative story

---
+++ grammar/stories/misc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  relative: [
+    "brother",
+    "sister",
+    "twin",
+    "father",
+    "mother",
+    "uncle",
+    "aunt",
+    "cousin",
+    "sibling"
+  ],
+
+  contest: [
+    "battle which lasted #takesTime#",
+    "game of #game#",
+    "battle of wills",
+    "public vote",
+    "tug-of-war",
+    "wrestling match",
+    "arm-wrestling contest",
+    "burrito challenge",
+    "coin-flip"
+  ]
+};
