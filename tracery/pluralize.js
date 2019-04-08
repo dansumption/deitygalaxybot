3a9a37d87d42799750772594809c5db407d43551
Dan Sumption <dan.sumption@autodesk.com>
2019-04-08 23:06:20 +0100

    Add plural rule for birds of paradise

--- tracery/pluralize.js
+++ tracery/pluralize.js
@@ -1,6 +1,7 @@
 const pluralize = require("pluralize");
 
 pluralize.addPluralRule(/^game of/i, "games of");
+pluralize.addPluralRule(/^bird of/i, "birds of");
 pluralize.addPluralRule(/ssus$/i, "ssi");
 pluralize.addPluralRule(/goose$/i, "geese");
 pluralize.addUncountableRule("thyme");
