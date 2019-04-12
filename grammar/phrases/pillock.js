63233f3013f2de77c5b232b823ef6f220849cbca
Dan Sumption <dan.sumption@autodesk.com>
2019-04-12 03:49:03 +0100

    Stick a little Dali in

--- grammar/phrases/pillock.js
+++ grammar/phrases/pillock.js
@@ -1,3 +1,7 @@
 module.exports = {
-  statements: ["Magic Magid for Galactic President!", "What? Me? Bovvered?"]
+  statements: [
+    "Magic Magid for Galactic President!",
+    "What? Me? Bovvered?",
+    "the difference between me and a madman is that I am not mad"
+  ]
 };
