30e2c745480fe8f1310d4a310ab7291aac159434
= <=>
2022-01-20 21:03:01 +0000

    Tidying and moving connect outside get/set

--- twit/database.js
+++ twit/database.js
@@ -1,33 +1,14 @@
 const redis = require("redis");
-const url = require("url");
 const { promisify } = require("util");
 
 const EXPIRY_TIME = 60 * 60 * 24 * 2; // expire after 2 days
 
-const redisURL = url.parse(
-  process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379"
-);
-const client = redis.createClient({ url: process.env.REDISCLOUD_URL });
-
-// const redisURL = url.parse(
-//   process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379"
-// );
-// const client = redis.createClient(redisURL.port, redisURL.hostname, {
-//   no_ready_check: true,
-//   legacyMode: true
-// });
-// if (process.env.REDISCLOUD_URL) {
-//   await client.connect();
-//   client.auth(redisURL.auth.split(":")[1]);
-// }
-
-// const __set = promisify(client.set).bind(client);
-// const __get = promisify(client.get).bind(client);
+const redisURL = process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379";
+const client = redis.createClient({ url: redisURL });
+await client.connect();
 
 const set = async (key, value) => {
   try {
-    console.log("con to " + redisURL);
-    await client.connect();
     const data = await client.set(key, value, "EX", EXPIRY_TIME);
     console.log(`SET: ${key}\n\t${value}\n\t${data}`);
     return data;
@@ -38,7 +19,6 @@ const set = async (key, value) => {
 
 const get = async key => {
   try {
-    await client.connect();
     const data = await client.get(key);
     console.log(`GET: ${key}\n\t${data}`);
     return data;
