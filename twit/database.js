07b99c6fbae0ac86303c25f64eeedf84f75a67fd
= <=>
2022-01-20 18:55:41 +0000

    etc

--- twit/database.js
+++ twit/database.js
@@ -8,13 +8,12 @@ const redisURL = url.parse(
   process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379"
 );
 const client = redis.createClient(redisURL.port, redisURL.hostname, {
-  no_ready_check: true
+  no_ready_check: true,
+  legacyMode: true
 });
 if (process.env.REDISCLOUD_URL) {
   client.auth(redisURL.auth.split(":")[1]);
 }
-
-client.connect();
 
 const __set = promisify(client.set).bind(client);
 const __get = promisify(client.get).bind(client);
