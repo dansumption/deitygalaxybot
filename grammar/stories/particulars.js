b3cf84d68d5a71b8525e7852c9491d8430b6c7ac
= <=>
2022-01-21 17:31:31 +0000

    Add spirit animal to original stories

--- grammar/stories/particulars.js
+++ grammar/stories/particulars.js
@@ -1,7 +1,8 @@
 module.exports = [
-  // 'To perform obesiance to #deityFull# #followerAction#.\n\nReply to this tweet if you wish to further abase yourself to #deityName#',
-  // "To perform service to #deityFull# #deityRequest# your #anyNoun#.\n\nReply to this tweet for further ways in which you can venerate #deityName#",
-  "Those who anger #deityFull# are liable to be #deityPunishment#.\n\nTo find out how to avoid being #deityPunishment#, reply to this tweet."
-  // "#deityFull# exists purely #reason#.\n\nReply to this tweet for guidance from #deityName#.",
-  // "Those who value #deityDomain# should pledge their soul to #deityName#, for #deityName# is #deityType# of #deityDomain#.\n\nReply to this tweet if you wish to become #deityName#'s latest worshipper."
+  "To perform obesiance to #deityFull# #followerAction#.\n\nReply to this tweet if you wish to abase yourself to #deityName#",
+  "To perform service to #deityFull# #deityRequest# your #anyNoun#.\n\nReply to this tweet for additional ways in which you can venerate #deityName#",
+  "Those who anger #deityFull# are liable to be #deityPunishment#.\n\nTo find out how to avoid being #deityPunishment#, reply to this tweet.",
+  "#deityFull# exists purely #reason#.\n\nReply to this tweet for guidance from #deityName#.",
+  "Those who value #deityDomain# should pledge their soul to #deityName#, for #deityName# is #deityType# of #deityDomain#.\n\nReply to this tweet if you wish to become #deityName#'s latest worshipper.",
+  "#deityFull# has a spirit animal: #adjective.a# #spiritAnimal#.\n\nIf you would like your own pet #spiritAnimal#, just reply to this tweet. Terms, conditions, and devotional requirements apply."
 ];
