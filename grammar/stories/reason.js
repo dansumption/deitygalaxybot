7f5c606155559d44659dd75880179ff2069c5a89
= <=>
2022-01-21 15:46:44 +0000

    Beef up and tidy up reasons for doing things

--- grammar/stories/reason.js
+++ grammar/stories/reason.js
@@ -4,21 +4,25 @@ module.exports = {
     "to encourage the return of #animal.s# to #anyPlace#",
     "to guarantee #adjective.a# #festival#",
     "to create a sense of #feeling#",
-    "because #deityThey# felt like it",
     "to bring #quality# to #nounToPluralise.s#",
     "to bring #nounNotToPluralise# to #nounToPluralise.s#",
     "as a substitute for #nounNotToPluralise#",
     "as a substitute #nounToPluralise#",
     "to pass time while cooking #foodStuff#",
-    "to appease #deityTheir# pet #spiritAnimal#",
-    "because only by doing so will the balance of the universe be restored",
+    "to appease #deityTheir# own pet #spiritAnimal#",
+    "because only by doing so will the sanctity of #nounNotToPluralise# be realised",
+    "because only by doing so will the platonic ideal of #nounToPluralise.s# become apparent",
+    "in order to teach a lesson to #deityTheir# own followers, who are too easily moved to #feeling#",
+    "in order to teach a lesson to #deityTheir# own followers, who are too enamoured of #belief#",
+    "in order to teach a lesson to #deityTheir# own followers, who spend too much time thinking about #nounNotToPluralise#",
+    "#rareReason#"
+  ],
+  rareReason: [
     "because DUH",
     "for reasons which ought to be obvious",
     "for the usual reasons",
     "because doing so might teach you a lesson",
-    "because of issues in #deityTheir# childhood",
-    "to teach a lesson to #deityTheir# followers, who are too easily moved to #feeling#",
-    "to teach a lesson to #deityTheir# followers, who are too enamoured of #belief#",
-    "to teach a lesson to #deityTheir# followers, who spend too much time thinking about #nounNotToPluralise#"
+    "because of issues in #deityTheir# own childhood",
+    "because #deityThey# felt like it"
   ]
 };
