a031325a16561e3c0a3adc6f4bf58bdb17912e2e
= <=>
2022-01-30 23:30:37 +0000

    More line breaks

---
+++ grammar/stories/storyChunks.js
@@ -0,0 +1,13 @@
+module.exports = {
+  deityClause: [
+    "#deityName#,\n#deityType.capitalize#\nOf #deityDomain.capitalize#",
+    "#deityType.capitalize#\nOf #deityDomain.capitalize#,\n#deityName#,"
+  ],
+  actionClause: [
+    "Watches #deityTheir# #spiritAnimal#\n#animalAction.capitalize# #adverb#",
+    "#deliverMessage.capitalize#\n#quest.capitalize#",
+    "#adverb.capitalize# #meditationAction#\n#adjective.a.capitalize# #anyNoun#",
+    "#adverb.capitalize# #toyWith#\n#adjective.capitalize# #anyNoun#",
+    ""
+  ]
+};
