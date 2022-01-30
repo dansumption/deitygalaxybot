9a11590fe21958f07c404c52401c468d74173c28
= <=>
2022-01-30 21:24:28 +0000

    Tidy debug

--- debug.js
+++ debug.js
@@ -13,7 +13,7 @@ const templateForceText = "After #takesTime#, #instructing#\n";
 const templateForceReply = "#[deityName:Waï-ceizsteuach]replyWithDeity#\n";
 
 let template = argv.template || "#origin#\n\n\n";
-template = complex; // uncomment to use template from separate file debugComplex.js
+// template = complex; // uncomment to use template from separate file debugComplex.js
 // template = templateForceText; // uncomment to force the above template
 // template = templateForceReply; // uncomment to force the above reply template
 // template = templateWithoutReply; // uncomment to force the above reply template
