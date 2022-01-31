67ff88a97691f18cb39084d232519a59209197ee
= <=>
2022-01-31 12:58:57 +0000

    Fix

--- grammar/stories/storyChunks.js
+++ grammar/stories/storyChunks.js
@@ -3,7 +3,7 @@ module.exports = {
     "Our #deityName#,\n#deityType.capitalize#\nOf #deityDomain.capitalize#",
     "The #deityType.capitalize#\nOf #deityDomain.capitalize#,\nNamed #deityName#",
     "Our #deityType.capitalize#\nOf our #deityDomain.capitalize#,\nWhom we name #deityName#",
-    "Their #deityType.capitalize#\nOf our #deityDomain.capitalize#,\nWhom they name #deityName#",
+    "Their #deityType.capitalize#\nOf their #deityDomain.capitalize#,\nWhom they name #deityName#",
     "The one named #deityName#,\n#deityType.capitalize#\nOf #deityDomain.capitalize#",
     "Sacred #deityName#,\nOh #deityType.capitalize#\nOf all #deityDomain.capitalize#"
   ],
