8da1ea75662bbe19085615aa6b114316bc1aeafb
= <=>
2022-01-31 00:35:42 +0000

    better actions

--- grammar/stories/storyChunks.js
+++ grammar/stories/storyChunks.js
@@ -5,14 +5,14 @@ module.exports = {
     "The #deityType.capitalize#\nOf #deityDomain.capitalize#,\nNamed #deityName#",
     "Our #deityType.capitalize#\nOf our #deityDomain.capitalize#,\nWhom we name #deityName#",
     "The one named #deityName#,\n#deityType.capitalize#\nOf #deityDomain.capitalize#",
-    "Sacred #deityName#,\nOh #deityType.capitalize#\nOf all #deityDomain.capitalize#",
-    ""
+    "Sacred #deityName#,\nOh #deityType.capitalize#\nOf all #deityDomain.capitalize#"
   ],
   actionClause: [
     "Watches #deityTheir# #spiritAnimal#\n#animalAction.capitalize# #adverb#",
     "#deliverMessage.capitalize#\n#quest.capitalize#",
     "#adverb.capitalize# #meditationAction#\n#adjective.a.capitalize# #anyNoun#",
     "#adverb.capitalize# #toyWith#\n#adjective.capitalize# #anyNoun#",
-    "#twoPersonAction#\nWith you"
+    "#twoPersonAction.capitalize#\n#nounToPluralise.s.capitalize#\nWith you",
+    "Is eagerly #making#"
   ]
 };
