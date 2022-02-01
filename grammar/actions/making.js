69a0dfb2ce12836ae0f5bbeb615574d01dd017c1
= <=>
2022-02-01 14:19:07 +0000

    Some more foodstuffs

--- grammar/actions/making.js
+++ grammar/actions/making.js
@@ -1,22 +1,24 @@
 module.exports = {
   making: [
-    "spinning a web\nFrom #buildingMaterial#",
+    "3D printing\n#nounToPluralise.a.capitalize#\nRecycled from\n#buildingMaterial.capitalize#",
+    "building a simulacrum\nOf #deityThem#self\nOut of #buildingMaterial#",
+    "constructing #dwelling.a#\nOut of #buildingMaterial#",
     "crocheting a blanket\nMade of #buildingMaterial#",
     "erecting #worshipPlace.a#\nMade of #buildingMaterial#",
-    "constructing #dwelling.a#\nOut of #buildingMaterial#",
-    "building a simulacrum\nOf #deityThem#self\nOut of #buildingMaterial#",
-    "sculpting idols\nFrom #buildingMaterial#",
-    "forming a new race\nOf intelligent\n#spiritAnimal.s.capitalize#\nout of #buildingMaterial#",
-    "kneading a knot\nOf #buildingMaterial#\nInto life",
     "finding a mate\nFor #deityTheir# #spiritAnimal#\nIn some\n#buildingMaterial.capitalize#",
+    "forming a new race\nOf intelligent\n#spiritAnimal.s.capitalize#\nout of #buildingMaterial#",
     "growing #flower.s#\nAnd #flower.s#\nIn a compost of\n#buildingMaterial.capitalize#",
-    "vacuum-forming\nA model-#dwelling#\nMade out of\n#buildingMaterial.capitalize#",
-    "sculpting #nounToPluralise.a#\nOut of #buildingMaterial#",
-    "packing wild #flower# seeds\nIn #buildingMaterial#\nCompost bombs",
     "inventing #newTech#\nBy abstracting\n#buildingMaterial.capitalize#",
     "investigating what\nCan be made by combining\n#buildingMaterial.capitalize#\nWith #buildingMaterial#",
+    "kneading a knot\nOf #buildingMaterial#\nInto life",
+    "packing wild #flower# seeds\nIn #buildingMaterial#\nCompost bombs",
+    "painting a picture of #anyNoun#\nWith #adjective# paint\nGround from\n#buildingMaterial.capitalize#",
+    "sculpting #nounToPluralise.a#\nOut of #buildingMaterial#",
+    "sculpting idols\nFrom #buildingMaterial#",
+    "spinning a web\nFrom #buildingMaterial#",
     "stirring a cauldron\nOf bubbling #buildingMaterial#",
-    "3D printing\n#nounToPluralise.a.capitalize#\nRecycled from\n#buildingMaterial.capitalize#",
-    "painting a picture of #anyNoun#\nWith #adjective# paint\nGround from\n#buildingMaterial.capitalize#"
+    "vacuum-forming\nA model-#dwelling#\nMade out of\n#buildingMaterial.capitalize#",
+    "crafting #singleNoun# from #buildingMaterial#",
+    "whipping up a #anyFood# out of #foodItem# and #buildingMaterial#"
   ]
 };
