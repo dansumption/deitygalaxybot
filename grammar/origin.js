d083d4357abd2b80f7560189edc33f87393210b5
= <=>
2022-02-02 18:07:16 +0000

    Cargo-culting

--- grammar/origin.js
+++ grammar/origin.js
@@ -2,11 +2,13 @@ const macros = require("./macros");
 
 const origin = {
   origin: [`${macros}#setDeity##setCharacter##initialStory#\n\n#replyPrompt#`],
-  replyOrigin: [`${macros}#userHandle#\n#initialStory#\n\n#initialReply#`],
+  replyOrigin: [
+    `${macros}#setDeity##userHandle#\n#initialStory#\n\n#initialReply#`
+  ],
   replyWithDeity: [`${macros}#userHandle#\n#replyStory#`],
   replyWithLostDeity: [
     // TODO: add more variety beyond the "another deity has taken their place" schtick.
-    `${macros}#userHandle# You call your deity, but another has taken their place. #origin#`
+    `${macros}#setDeity##userHandle# You call your deity, but another has taken their place. #origin#`
   ],
   searchOrigin: [
     `${macros}Deep beneath the Earth, the goddess Rheða sighs, and the ground around #placeName# quivers, giving #userHandle# belly-butterflies. It must be #today#.`
