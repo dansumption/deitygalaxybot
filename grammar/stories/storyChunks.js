46f78e3990d974648edbcdce3b17af95f6a79958
Dan Sumption <dan@sumption.org>
2022-02-04 02:59:40 +0000

    update storyChunks.js

--- grammar/stories/storyChunks.js
+++ grammar/stories/storyChunks.js
@@ -1,10 +1,10 @@
 module.exports = {
   deityClause: [
-    "Our #deityName#,\n#deityType.capitalize#\nOf #deityDomain.capitalize#",
+    "Our #deityName#,\n#adjective.capitalize# #deityType.capitalize#\nOf #deityDomain.capitalize#",
     "The #deityType.capitalize#\nOf #deityDomain.capitalize#,\nNamed #deityName#",
     "Our #deityType.capitalize#\nOf our #deityDomain.capitalize#,\nWhom we name #deityName#",
-    "Their #deityType.capitalize#\nOf their #deityDomain.capitalize#,\nWhom they name #deityName#",
-    "The one named #deityName#,\n#deityType.capitalize#\nOf #deityDomain.capitalize#",
+    "Their #deityType.capitalize#\nOf #deityDomain.capitalize#,\nWhom they name #deityName#",
+    "The one named #deityName#,\n#adjective.capitalize# #deityType.capitalize#\nOf #deityDomain.capitalize#",
     "Sacred #deityName#,\nOh #deityType.capitalize#\nOf all #deityDomain.capitalize#"
   ],
   actionClause: [
